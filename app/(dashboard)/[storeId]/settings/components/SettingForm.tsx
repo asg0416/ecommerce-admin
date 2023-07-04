30e9a1b2835db98e4843eeec310816bd6e435993
sujin <sujin@sujinui-MacBookAir.local>
2023-07-04 23:41:24 +0900

    setting page 스토어 삭제, 스토어명 수정 화면 작성

--- app/(dashboard)/[storeId]/settings/components/SettingForm.tsx
+++ app/(dashboard)/[storeId]/settings/components/SettingForm.tsx
@@ -1,18 +1,94 @@
 "use client";
 
+import * as zod from "zod";
 import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Fragment, useState } from "react";
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingFormProps {
   initialData: Store;
 }
 
+// db store 컬럼 속성 name으로 지정해서 name 맞춰야함.
+const formSchema = zod.object({
+  name: zod.string().min(1),
+});
+
+type SettingFormValues = zod.infer<typeof formSchema>;
+
 const SettingForm: React.FC<SettingFormProps> = ({ initialData }) => {
-  return <div className="flex items-center justify-between">
-    {/* <Heading
-    title="Settings"
-    description="Manage store preferences"
-    /> */}
-    </div>;
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmitHandler = async (data: SettingFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <Fragment>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button
+          disabled={loading}
+          variant="destructive"
+          size="icon"
+          onClick={() => setOpen(true)}
+        >
+          <Trash className=" h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmitHandler)}
+          className=" space-y-8 w-full"
+        >
+          <div className=" grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className=" ml-auto " type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
+    </Fragment>
+  );
 };
 
 export default SettingForm;

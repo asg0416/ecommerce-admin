023a787d60c0c7f3b790f8c2c5c90d49c00550a7
sujin <sujin@sujinui-MacBookAir.local>
2023-07-07 00:38:24 +0900

    billboard model 생성 및 db 적용, billboard 생성 및 세팅 페이지 구현, 이미지 등록 라이브러리 추가 및 기능 구현

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -20,6 +20,11 @@ const MainNav = ({
       active: pathname === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Setting",
       active: pathname === `/${params.storeId}/settings`,
